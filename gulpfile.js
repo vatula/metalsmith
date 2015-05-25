c8898b059173583a863f843679072ba9825bccb8
Bogdan Vatulya <bogdan.watula@gmail.com>
2015-05-25 21:19:01 +1000

    enabled experimental es7 support

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ gulp.task('clear', function() {
 
 gulp.task('default', ['clear'], function() {
     return gulp.src('src/metalsmith.js')
-        .pipe(babel())
+        .pipe(babel({stage: 1}))
         .pipe(rename({basename: 'index'}))
         .pipe(gulp.dest('lib'));
 });
